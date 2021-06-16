a19a969f3e635599e7a461961afaca3bab923def
Nimesh Srivastava <snimesh412@gmail.com>
2021-06-16 11:02:28 +0530

    add file

---
+++ Practice/2num.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n1, n2;
+        cin>>n1>>n2;
+        cout<<n1+n2<<"\n";
+    }
+    return 0;
+}
